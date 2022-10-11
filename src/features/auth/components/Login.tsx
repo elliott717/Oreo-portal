f0330d2d6f72feda88263535e5d7c03788190059
elliott717 <elliott717@gmail.com>
2022-10-11 17:48:37 +0800

    auth0 provider

--- src/features/auth/components/Login.tsx
+++ src/features/auth/components/Login.tsx
@@ -1,13 +1,9 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import {toAbsoluteUrl} from 'helpers'
-
-/*
-  Formik+YUP+Typescript:
-  https://jaredpalmer.com/formik/docs/tutorial#getfieldprops
-  https://medium.com/@maurice.de.beijer/yup-validation-and-typescript-and-formik-6c342578a20e
-*/
+import { useAuth0 } from "@auth0/auth0-react";
 
 export function Login() {
+  const { loginWithRedirect } = useAuth0();
   return (
     <>
       {/* begin::Heading */}
@@ -19,9 +15,8 @@ export function Login() {
       {/* begin::Action */}
       <div className='text-center'>
         <button
-          type='submit'
-          id='kt_sign_in_submit'
           className='btn btn-lg btn-primary w-100 mb-5'
+          onClick={() => loginWithRedirect()}
         >
         Login
         </button>
@@ -38,28 +33,6 @@ export function Login() {
             className='h-20px me-3'
           />
           Continue with Google
-        </a>
-        {/* end::Google link */}
-
-        {/* begin::Google link */}
-        <a href='#' className='btn btn-flex flex-center btn-light btn-lg w-100 mb-5'>
-          <img
-            alt='Logo'
-            src={toAbsoluteUrl('/media/svg/brand-logos/facebook-4.svg')}
-            className='h-20px me-3'
-          />
-          Continue with Facebook
-        </a>
-        {/* end::Google link */}
-
-        {/* begin::Google link */}
-        <a href='#' className='btn btn-flex flex-center btn-light btn-lg w-100'>
-          <img
-            alt='Logo'
-            src={toAbsoluteUrl('/media/svg/brand-logos/apple-black.svg')}
-            className='h-20px me-3'
-          />
-          Continue with Apple
         </a>
         {/* end::Google link */}
       </div>
